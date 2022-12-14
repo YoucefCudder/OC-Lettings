8512b1616ab63887dcd67111412581786fd126f3
Youcef Aouali <you.aouali@gmail.com>
2022-12-14 16:30:32 +0100

    tests unitaires à finir

--- lettings/tests.py
+++ lettings/tests.py
@@ -1,3 +1,36 @@
-from django.test import TestCase
+import unittest
 
-# Create your tests here.
+from django.urls import reverse
+
+from .models import Letting
+from django.test import RequestFactory, Client, TestCase
+
+
+class TestLetting(TestCase):
+    client = Client()
+
+    def test_lettings(self):
+        # Call the method under test by generating the URL using the reverse function
+        response = self.client.get(reverse('lettings_index'))
+
+        # Assert that the response has a 200 status code
+        self.assertEqual(response.status_code, 200)
+
+        # Assert that the correct template was used to render the response
+        self.assertTemplateUsed(response, 'lettings_index.html')
+
+    def test_letting(self):
+        # Call the method under test by generating the URL using the reverse function
+        response = self.client.get(reverse('letting', kwargs={'letting_id': self.client}))
+
+        # Assert that the response has a 200 status code
+        self.assertEqual(response.status_code, 200)
+
+        # Assert that the correct template was used to render the response
+        self.assertTemplateUsed(response, 'letting.html')
+
+        # Check that the correct letting was passed to the template
+        # self.assertEqual(self.letting.title, response.context['title'])
+        # self.assertEqual(self.letting.address, response.context['address'])
+        self.assertIn(b'<title>Oceanview Retreat</title>', response.content)
+
--- profiles/tests.py
+++ profiles/tests.py
@@ -1,3 +1,34 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from django.urls import reverse
 
-# Create your tests here.
+from .models import Profile
+
+
+class TestProfileViews(TestCase):
+    client = Client()
+
+    def test_profiles_index(self):
+        # Call the method under test by generating the URL using the reverse function
+        response = self.client.get(reverse('profiles_index'))
+
+        # Assert that the response has a 200 status code
+        self.assertEqual(response.status_code, 200)
+
+        # Assert that the correct template was used to render the response
+        self.assertTemplateUsed(response, 'profiles_index.html')
+
+        # Check that the profiles list is correct
+        self.assertIn(self.client, response.context['profiles_list'])
+
+    def test_profile(self):
+        # Call the method under test by generating the URL using the reverse function
+        response = self.client.get(reverse('profile', kwargs={'username': self.client.user.username}))
+
+        # Assert that the response has a 200 status code
+        self.assertEqual(response.status_code, 200)
+
+        # Assert that the correct template was used to render the response
+        self.assertTemplateUsed(response, 'profile.html')
+
+        # Check that the correct profile was passed to the template
+        self.assertEqual(self.client, response.context['profile'])
