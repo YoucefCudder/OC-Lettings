00f81997c8f98c97237ef280a48f0be9ca421023
Youcef Aouali <you.aouali@gmail.com>
2023-01-31 16:22:06 +0100

    TEST FAIL LINTING

--- oc_lettings_site/views.py
+++ oc_lettings_site/views.py
@@ -1,8 +1,7 @@
 from django.shortcuts import render
 
 
-# Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-# Quisque molestie quam lobortis leo consectetur
+# Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque molestie quam lobortis leo consectetur
 # ullamcorper non id est. Praesent dictum, nulla eget feugiat sagittis,
 # sem mi convallis eros, vitae dapibus nisi
 # lorem dapibus sem. Maecenas pharetra purus ipsum, eget consequat ipsum lobortis quis.
